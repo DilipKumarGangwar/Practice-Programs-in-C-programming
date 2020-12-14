4325180750d643068b2d92e119b9dd13f16d64af
DilipKumarGangwar <gangwardilipkumar@gmail.com>
2020-12-14 09:24:49 +0530

    commit

--- Number_is_Even_or_odd_Using_Bitwise_Operators.c
+++ Number_is_Even_or_odd_Using_Bitwise_Operators.c
@@ -1,5 +1,5 @@
-//C program to check Whether a number is even or odd 
-//using Bitwise Operators (&,|,^)
+//C program to check Whether a number is even or odd using Bitwise Operators (&,|,^)
+
 #include<stdio.h>
 int main()
 {
@@ -8,17 +8,20 @@ int main()
     printf("Enter a number");
     scanf("%d",&n);
     //Processing  , Output
+    //Using BITWISE AND
     if((n&1)==0)
-      printf("%d is Even no",n);
+      printf("%d is Even no\n",n);
     else 
-      printf("%d is Odd no",n);  
+      printf("%d is Odd no\n",n);
+    //Using BITWISE OR    
     if((n|1)==(n+1))
-      printf("%d is Even no",n);
+      printf("%d is Even no\n",n);
     else 
-      printf("%d is Odd no",n);  
+      printf("%d is Odd no\n",n);  
+    //Using BITWISE XOR      
     if((n^1)==(n+1))
-      printf("%d is Even no",n);
+      printf("%d is Even no\n",n);
     else 
-      printf("%d is Odd no",n);  
+      printf("%d is Odd no\n",n);  
     return 0;
 }
