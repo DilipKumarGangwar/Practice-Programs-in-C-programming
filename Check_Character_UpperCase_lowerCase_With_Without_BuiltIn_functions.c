929cb758cdb3600eba6e951e6961ba846902f99d
DilipKumarGangwar <gangwardilipkumar@gmail.com>
2021-01-13 23:20:16 +0530

    commit

---
+++ Check_Character_UpperCase_lowerCase_With_Without_BuiltIn_functions.c
@@ -0,0 +1,55 @@
+//Approach 1:Program to Check Character is in UpperCase or in lowerCase(using Nested if else)
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter character:");
+    scanf("%c",&ch);
+    if(ch>='A'&&ch<='Z')  //check for uppercase
+        printf("%c is in Uppercase",ch);
+    else
+    {
+        if(ch>='a'&&ch<='z') //check for lowercase
+          printf("%c is in Lowercase",ch);
+        else                   //Invalid Input  
+            printf("Invalid Input"); 
+    }
+    return 0;
+}
+
+
+
+//Approach 2:Program to Check Character is in UpperCase or in lowerCase(using else if)
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter character:");
+    scanf("%c",&ch);
+    if(ch>='A'&&ch<='Z')  //check for uppercase
+        printf("%c is in Uppercase",ch);
+    else if(ch>='a'&&ch<='z') //check for lowercase
+        printf("%c is in Lowercase",ch);
+    else                   //Invalid Input  
+        printf("Invalid Input"); 
+    return 0;
+}
+
+
+
+//Approach-3: Check Character is in UpperCase or in lowerCase(using Built In functions)
+#include<stdio.h>
+#include<ctype.h>  //header file for declaration of isupper(), islower()
+int main()
+{
+    char ch;
+    printf("Enter character:");
+    scanf("%c",&ch);
+    if(isupper(ch)>0)  //check for uppercase
+        printf("%c is in Uppercase",ch);
+    else if(islower(ch)>0) //check for lowercase
+        printf("%c is in Lowercase",ch);
+    else                   //Invalid Input  
+        printf("Invalid Input"); 
+    return 0;
+}
