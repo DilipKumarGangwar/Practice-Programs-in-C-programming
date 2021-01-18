15604301b652c6784efea52b64cd177643882e9e
DilipKumarGangwar <gangwardilipkumar@gmail.com>
2021-01-18 11:03:49 +0530

    commit

---
+++ Calculator_Using Switch_Case.c
@@ -0,0 +1,90 @@
+//Calculator using switch-case (Using Choice as Integer)
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    int choice;
+    //print choices
+    printf("Choices are:\n");
+    printf("1.Addition 2.Subtraction 3.Multiplication 4.Division\n");
+    //scan the numbers
+    printf("Enter 2 numbers: ");
+    scanf("%f%f",&a,&b);
+    //scan the user's choice
+    printf("Enter the desired operation to do: ");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+      //cases with error handling 
+       
+       //Addition
+       case 1:
+              printf("Addition of %f and %f=%f",a,b,a+b);
+              break;
+       //Sub
+       case 2:
+             printf("Subtraction of  %f and %f=%f",a,b,a-b);
+             break;  
+       //Mult
+       case 3:
+             printf("Multiplication of  %f and %f=%f",a,b,a*b);
+             break;
+       //Div
+       case 4:
+             if(b==0)
+               printf("Division By Zero error");
+             else  
+              printf("Division of  %f and %f=%f",a,b,a/b);
+             break;
+       default:
+             printf("Please Enter Right Choice");      
+            
+    }
+    return 0;
+}
+
+//Calculator using switch-case (Using Choice as Character + - * /)
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    char choice;
+    //print choices
+    printf("Choices are:\n");
+    printf("1.Addition 2.Subtraction 3.Multiplication 4.Division\n");
+    //scan the numbers
+    printf("Enter 2 numbers: ");
+    scanf("%f%f",&a,&b);
+    //scan the user's choice
+    fflush(stdin);
+    printf("Enter the desired operation to do: ");
+    scanf("%c",&choice);
+    switch(choice)
+    {
+      //cases with error handling 
+       
+       //Addition
+       case '+':
+              printf("Addition of %f and %f=%f",a,b,a+b);
+              break;
+       //Sub
+       case '-':
+             printf("Subtraction of  %f and %f=%f",a,b,a-b);
+             break;  
+       //Mult
+       case '*':
+             printf("Multiplication of  %f and %f=%f",a,b,a*b);
+             break;
+       //Div
+       case '/':
+             if(b==0)
+               printf("Division By Zero error");
+             else  
+              printf("Division of  %f and %f=%f",a,b,a/b);
+             break;
+       default:
+             printf("Please Enter Right Choice");      
+            
+    }
+    return 0;
+}
--- Convert_UpperCase_to_lowerCase_and_ViceVersa-With_without_Library_Fn.c
+++ Convert_UpperCase_to_lowerCase_and_ViceVersa-With_without_Library_Fn.c
@@ -1,47 +1,6 @@
-//Comment necessary parts to run the program
-//Approach 1:Program to Check Character is in UpperCase or in lowerCase(using Nested if else)
-#include<stdio.h>
-int main()
-{
-    char ch;
-    printf("Enter character:");
-    scanf("%c",&ch);
-    if(ch>='A'&&ch<='Z')  //check for uppercase
-        printf("Lowercase form:%c",ch+32);
-    else
-    {
-        if(ch>='a'&&ch<='z') //check for lowercase
-          printf("Uppercase form:%c",ch-32);
-        else                   //Invalid Input  
-            printf("Invalid Input"); 
-    }
-    return 0;
-}
-
-
-//Approach 2:Program to Check Character is in UpperCase or in lowerCase(using else if)
-#include<stdio.h>
-int main()
-{
-    char ch;
-    printf("Enter character:");
-    scanf("%c",&ch);
-    if(ch>='A'&&ch<='Z')  //check for uppercase
-        printf("Lowercase form:%c",ch+32);
-    else if(ch>='a'&&ch<='z') //check for lowercase
-        printf("Uppercase form:%c",ch-32);
-    else                   //Invalid Input  
-        printf("Invalid Input"); 
-    return 0;
-}
-
-
-
 //Approach-3: Check Character is in UpperCase or in lowerCase(using Built In functions)
-//isupper() returns integer value > 0 ,if character is in uppercase , else returns 0
-//islower() returns integer value > 0 ,if character is in lowercase , else returns 0
 #include<stdio.h>
-#include<ctype.h>  //header file for declaration of isupper(), islower()
+#include<ctype.h>  //header file for declaration of isupper(), islower(),tolower(),toupper()
 int main()
 {
     char ch;
@@ -55,3 +14,4 @@ int main()
         printf("Invalid Input"); 
     return 0;
 }
+
